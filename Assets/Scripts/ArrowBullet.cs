3a494c8368e8e1a1c4c141eace421f27ff802e3e
roland006 <roland006@rambler.ru>
2020-06-27 10:55:18 +0300

    Ф

---
+++ Assets/Scripts/ArrowBullet.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowBullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.layer == 9)
+        {
+            Destroy(coll.gameObject);
+        }
+        if (coll.gameObject.layer == 10)
+        {
+           // GetComponentInParent<Rigidbody>().freezeRotation = true;
+           // GetComponentInParent<Rigidbody>().transform.position = transform.position;
+            GetComponentInParent<Rigidbody>().Sleep();
+        }
+    }
+    /*void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == 9)
+        {
+            Destroy(collision.gameObject);
+        }
+    } */
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
--- Assets/Scripts/LookAtMouse.cs
+++ Assets/Scripts/LookAtMouse.cs
@@ -1,43 +1,66 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
 
 public class LookAtMouse : MonoBehaviour
 {
+    private Vector3 BowPos;
+    
+    public GameObject bulletPrefab;
+    public Transform gunPoint;
+    public int bulletSpeedMult = 10;
+    private float curTimeout;
+    private float bulletSpeed;
+    private Vector3 targetPos;
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if(Input.GetMouseButton(0))
         {
-            StartCoroutine(Look());
+            
         }
-    }
-
-    IEnumerator Look()
-    {
-        yield return new WaitForSeconds(0.00001f);
-        var mousePosition = Input.mousePosition;
-        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-        if (mousePosition.z <= 1 && mousePosition.x >= -1 && mousePosition.x <= 1)
+        if (Input.GetButton("Fire1"))
         {
-            while (Input.GetButton("Fire1"))
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
             {
-                mousePosition = Input.mousePosition;
-                mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-                Vector3 xzPos = mousePosition - transform.position;
-                xzPos.y = 0;
-                var angle = Vector3.Angle(Vector3.back, xzPos);
-                if(tag == "Arrow")
+                bulletSpeed = Mathf.Abs(BowPos.x - hit.point.x) + Mathf.Abs(BowPos.z - hit.point.z);
+                Debug.Log("Speed = "+ Mathf.Abs(BowPos.x - hit.point.x) + Mathf.Abs(BowPos.z - hit.point.z));
+                if (Mathf.Abs(BowPos.x - hit.point.x) > 0.1 || Mathf.Abs(BowPos.z - hit.point.z) > 0.1)
                 {
-                    Debug.Log(transform.position);
-                    transform.position = new Vector3(mousePosition.x, 0f, mousePosition.z);
-                    
-
-                    Debug.Log(mousePosition);
+                    Vector3 HitPos = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+                    targetPos = HitPos;
+                    transform.LookAt(HitPos);
                 }
-                transform.eulerAngles = new Vector3(transform.eulerAngles.x, 0f, mousePosition.x > 0 ? angle : -angle);
-                yield return new WaitForSeconds(0.00001f);
+                else
+                {
+                    //transform.rotation = Quaternion.LookRotation(Vector3.back);
+                }
             }
         }
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.gameObject.layer == 8)
+                {
+                    BowPos = new Vector3(hit.point.x, hit.point.y, hit.point.z);
+                    transform.position = BowPos;
+                }
+            }
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            GameObject bulletInstance = Instantiate(bulletPrefab,gunPoint.position,gunPoint.rotation) as GameObject;
+            Rigidbody instBulletRigidbody = bulletInstance.GetComponent<Rigidbody>();
+            instBulletRigidbody.centerOfMass = new Vector3(0,0,0.5f);
+            instBulletRigidbody.AddForce(transform.forward * bulletSpeed * bulletSpeedMult*-1);
+            
+        }
     }
+
 
 }
